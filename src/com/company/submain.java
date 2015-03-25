e5b4f612c93ce51d7889c42b6fb8736f75d40759
Admin <affrica514@gmail.com>
2015-03-25 21:02:59 +0200

    add submain class

---
+++ src/com/company/submain.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by Admin on 25.03.15.
+ */
+public class submain {
+    public static void main(String[] args) {
+     /*   if (args[0].equals("hello")) {
+            System.out.println("Class");
+        } else {
+            System.out.println("Good bye");
+        }
+     */
+
+        if (args[0].equals("hello") && args[1].equals("Class")) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Good bye");
+        }
+
+    }
+}
