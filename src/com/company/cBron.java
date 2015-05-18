c40468c61f42a00f37883019ce7bb9c054f04ed2
Admin <affrica514@gmail.com>
2015-05-18 21:10:56 +0300

    UZ

---
+++ src/com/company/cBron.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * Created by Admin on 18.05.15.
+ */
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class cBron {
+    public static void EnterFrom(WebDriver d2, String s) {
+        WebElement w = d2.findElement(By.xpath("//div[@id='station_from']//div[@title='"+s+"']"));
+        w.sendKeys(Keys.ENTER);
+    }
+    public static void EnterTo(WebDriver d2, String s) {
+        WebElement w = d2.findElement(By.xpath("//div[@id='station_till']//div[@title='"+s+"']"));
+        w.sendKeys(Keys.ENTER);
+    }
+    public static void DoSearch(WebDriver d2) {
+        WebElement w = d2.findElement(By.xpath("//button[@name='search']"));
+        w.sendKeys(Keys.ENTER);
+    }
+}
---
+++ src/com/company/cTestBron.java
@@ -0,0 +1,54 @@
+package com.company;
+
+/**
+ * Created by Admin on 18.05.15.
+ */
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+@RunWith(JUnit4.class)
+public class cTestBron {
+
+    public WebDriver d2;
+    @Test
+    public void ResurveTicket() {
+        cBron.EnterFrom(d2, "Kyiv");
+        cBron.EnterTo(d2, "Ivano-Frankivsk");
+//        cBron.EnterDate(d2);
+        cBron.DoSearch(d2);
+//        ViewResSearch(d2);
+//        PickResSearch(d2);
+//        PlaceIsFree(d2);
+//        TankNumberIs(d2);
+//        PickPlace(d2);
+//        CheckPrice(d2);
+        String expRes = "";
+        Assert.assertEquals("Fail", expRes, cTranslatePage.GetResult(d2));
+    }
+
+    @Before
+    public void start()
+    {   //System.setProperty("webdriver.chrome.driver", "/home/yana/QAFactory/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:/Automation/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
+        d2 = new ChromeDriver();
+        d2.manage().window().maximize();
+        d2.get("http://booking.uz.gov.ua/");
+    }
+
+    @After
+    public void clean()
+    {d2.quit();
+    }
+}
